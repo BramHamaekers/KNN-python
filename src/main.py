d9fea91b8094a02c6dde1c783c38fb0adc399fd5
BramHamaekers <bram.hamaekers@gmail.com>
2023-01-15 19:06:30 +0100

    refactor

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
-from src.KNN import KNN_model
+from src.KNN.KNN import KNN_model
 
 
 def load_iris():
