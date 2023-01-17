c94c3a1661e15b86d5867ae18669ff1cdc7ab9f6
BramHamaekers <bram.hamaekers@gmail.com>
2023-01-17 19:52:11 +0100

    D_TREE: implement information gain and variance

--- src/D_TREE/D_TREE.py
+++ src/D_TREE/D_TREE.py
@@ -1,9 +1,26 @@
 from math import log2
 
 
-def entropy(dataset):
-    p_list = dataset.value_counts() / dataset.shape[0]
+def variance(Y_train):
+    return Y_train.var()
+
+
+def entropy(Y_train):
+    p_list = Y_train.value_counts() / Y_train.shape[0]
     return -sum(p * log2(p) for p in p_list)
+
+
+def information_gain(X_train, Y_train, test):
+    # test must be binary test
+    # si = |S_i| / |S|
+    s1 = sum(test)/X_train.shape[0]
+    s2 = 1 - s1
+
+    # IG_Regression(S,t) = Var(S) - sum((|S_i| / |S|)*Var(S_i)
+    # IG_Classification(S,t) = CE(S) - sum((|S_i| / |S|)*CE(S_i)
+    if s1 == 0 or s2 == 0: return 0
+    if Y_train.dtypes != 'object': variance(Y_train) - (s1 * variance(Y_train[test])) - (s2 * variance(Y_train[-test]))
+    return entropy(Y_train) - (s1 * entropy(Y_train[test])) - (s2 * entropy(Y_train[-test]))
 
 
 class D_TREE_model:
--- test/test_D_TREE.py
+++ test/test_D_TREE.py
@@ -1,5 +1,8 @@
+import numpy as np
 import unittest
 import pandas as pd
+
+from src import datasets
 from src.D_TREE import D_TREE
 
 
@@ -16,6 +19,20 @@ class test_D_TREE(unittest.TestCase):
         self.assertEqual(round(D_TREE.entropy(case_2), 3), 0.650)
         self.assertEqual(D_TREE.entropy(case_3), 0)
         self.assertEqual(D_TREE.entropy(case_4), 1)
+
+    def test_variance(self):
+        None
+        # TODO
+
+    def test_information_gain(self):
+        # Create data
+        X_train, Y_train, X_test, Y_test = datasets.load_Obesity()
+        X = pd.concat([X_train, X_test], axis=0)
+        Y = pd.concat([Y_train, Y_test], axis=0)
+
+        # Test cases
+        ig = D_TREE.information_gain(X, Y, X['Gender'] == 'Male')
+        self.assertEqual(round(ig, 7), 0.0005507)
 
 
 
